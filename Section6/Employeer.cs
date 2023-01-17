9a7a97495c780759db3c09e0f26176318ba4ddc6
André <andrereis.1905@gmail.com>
2023-01-17 18:52:31 -0300

    Employeers

---
+++ Section6/Employeer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section6
+{
+    public class Employeer
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employeer()
+        {
+
+        }
+        public Employeer(int id, string name, double salary) : this()
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            var increase = (percentage / 100) + 1;
+            var result = Salary * increase;
+        }
+    }
+}
--- Section6/Program.cs
+++ Section6/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Section6;
+
+List<Employeer> list = new List<Employeer>();
+
+Console.Write("How many employees will be registred? ");
+int n = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Employee #" + (i + 1) + ";");
+    Console.Write("Id: ");
+    int id = int.Parse(Console.ReadLine());
+
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Salary: ");
+    double salary = double.Parse(Console.ReadLine());
+
+    list.Add(new Employeer(id, name, salary));
+}
+
+Console.Write("Enter the employee id that will have salary increase: ");
+double idAttempt = double.Parse(Console.ReadLine());
+var attempt = list.FirstOrDefault(employeer => employeer.Id == idAttempt);
+
+
+if (attempt != null)
+{
+    Console.Write("Enter the percentage: ");
+    double percentage = double.Parse(Console.ReadLine());
+    attempt.IncreaseSalary(percentage);
+}
+else
+    Console.WriteLine("This Id does not exist!");
+
+Console.WriteLine("Update list of employees:");
+foreach(Employeer obj in list)
+{
+    Console.Write(obj.Id + ", ");
+    Console.Write(obj.Name+ ", ");
+    Console.Write(obj.Salary);
+}
+
